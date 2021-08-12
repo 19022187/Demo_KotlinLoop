7bdbd7c1ce0ab1acfb40b8e2197600f15cfa1f67
19022187 <19022187@myrp.edu.sg>
2021-08-12 12:07:27 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demokotlinloop/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.myapplicationdev.android.demokotlinloop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCount
+        btnLetter
+        etWord
+
+        btnCount.setOnClickListener {
+            for (i in 1..5) {
+                Log.d("MainActivity:", i.toString())
+            }
+        }
+
+        btnLetter.setOnClickListener {
+            val letter = etWord.text.toString()
+            for (i in letter) {
+                Log.d("MainActivity:", i.toString())
+            }
+        }
+    }
+}
